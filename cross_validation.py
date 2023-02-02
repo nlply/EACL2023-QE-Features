335fe32a96f03326ba749a02f2fa0145a1b57a1c
yang liu <756493290@qq.com>
2023-02-03 00:50:35 +0800

    :sparkles: first commit

---
+++ cross_validation.py
@@ -0,0 +1,235 @@
+import numpy as np
+import pandas as pd
+from sklearn import svm
+from sklearn.metrics import precision_recall_fscore_support
+import torch
+import torch.nn as nn
+import itertools
+import logging
+import sys
+from time import strftime, localtime
+from nltk import word_tokenize
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def tokenize(q):
+    return word_tokenize(q.lower())
+
+def train_word2vec_svm_cv(dataset, k, word2vec_mat, features):
+    p_list = []
+    r_list = []
+    f_list = []
+    acc_list = []
+    N = dataset.shape[0]
+    index_perm = np.random.permutation(N).tolist()
+    index_start = list(range(0, N, N // k))[:k] + [N]
+    for i in range(k):
+        s = index_start[i]
+        t = index_start[i + 1]
+        index_test = index_perm[s:t]
+        index_train = index_perm[:s] + index_perm[t:]
+        train_dataset = dataset.iloc[index_train]
+        test_dataset = dataset.iloc[index_test]
+        if features == []:
+            X_train = []
+            y_train = []
+            X_test = []
+            y_test = []
+            for i in index_train:
+                X_train.append(list(word2vec_mat[i, :].tolist()))
+            y_train = list(train_dataset['label'])
+            for i in index_test:
+                X_test.append(list(word2vec_mat[i, :].tolist()))
+            y_test = list(test_dataset['label'])
+
+        else:
+            X_train = []
+            y_train = []
+            X_test = []
+            y_test = []
+            for i, j in zip(index_train, range(len(train_dataset))):
+                append_feature_list = []
+                for feat in features:
+                    append_feature_list.append(train_dataset.iloc[j][feat])
+                X_train.append(list(word2vec_mat[i, :].tolist()) + append_feature_list)
+            y_train = list(train_dataset['label'])
+            for i, j in zip(index_test, range(len(test_dataset))):
+                append_feature_list = []
+                for feat in features:
+                    append_feature_list.append(test_dataset.iloc[j][feat])
+                X_test.append(list(word2vec_mat[i, :].tolist()) + append_feature_list)
+            y_test = list(test_dataset['label'])
+        clf = svm.SVC(C=1.0, kernel='rbf', gamma='auto')
+        clf.fit(X_train, y_train)
+
+        y_pred = clf.predict(X_test)
+        acc = np.mean(y_test == y_pred)
+        p, r, f, _ = precision_recall_fscore_support(y_test, y_pred, average='macro')
+
+        p_list.append(p)
+        r_list.append(r)
+        f_list.append(f)
+        acc_list.append(acc)
+
+    return np.mean(p_list), np.mean(r_list), np.mean(f_list), np.mean(acc_list)
+
+
+def train_svm_cv(dataset, k, feature):
+    p_list = []
+    r_list = []
+    f_list = []
+    acc_list = []
+    N = dataset.shape[0]
+    index_perm = np.random.permutation(N).tolist()
+    index_start = list(range(0, N, N // k))[:k] + [N]
+
+    for i in range(k):
+        s = index_start[i]
+        t = index_start[i + 1]
+        index_test = index_perm[s:t]
+        index_train = index_perm[:s] + index_perm[t:]
+        df_train = dataset.iloc[index_train]
+        df_test = dataset.iloc[index_test]
+
+        if len(feature) == 1:
+            X_train = np.expand_dims(df_train[feature[0]].values, 1)
+            y_train = df_train['label'].values
+            X_test = np.expand_dims(df_test[feature[0]].values, 1)
+            y_test = df_test['label'].values
+        else:
+            X_train = df_train[feature].values
+            y_train = df_train['label'].values
+            X_test = df_test[feature].values
+            y_test = df_test['label'].values
+
+        clf = svm.SVC(C=1.0, kernel='rbf', gamma='auto')
+        clf.fit(X_train, y_train)
+
+        y_pred = clf.predict(X_test)
+
+        acc = np.mean(y_test == y_pred)
+        p, r, f, _ = precision_recall_fscore_support(y_test, y_pred, average='macro')
+
+        p_list.append(p)
+        r_list.append(r)
+        f_list.append(f)
+        acc_list.append(acc)
+
+    return np.mean(p_list), np.mean(r_list), np.mean(f_list), np.mean(acc_list)
+
+def getWord2id(df):
+    setups = df['set-up'].tolist()
+    punchlines = df['punchline'].tolist()
+    word2id = dict()
+    for setup, punchline in zip(setups, punchlines):
+        setup = tokenize(setup)
+        for s_word in setup:
+            s_word = s_word.lower().strip()
+            if word2id.get(s_word) == None:
+                word2id[s_word] = len(word2id)
+
+        punchline = tokenize(punchline)
+        for p_word in punchline:
+            p_word = p_word.lower().strip()
+            if word2id.get(p_word) == None:
+                word2id[p_word] = len(word2id)
+
+    return word2id
+
+
+def get_numpy_word_embed(word2ix):
+    row = 0
+    whole = 'glove.6B.50d.txt'
+    words_embed = {}
+    with open(whole, mode='r', encoding='utf-8')as f:
+        lines = f.readlines()
+        for line in lines:
+            line_list = line.split()
+            word = line_list[0]
+            embed = line_list[1:]
+            embed = [float(num) for num in embed]
+            words_embed[word] = embed
+            row += 1
+    ix2word = {ix: w for w, ix in word2ix.items()}
+    id2emb = {}
+    for ix in range(len(word2ix)):
+        if ix2word[ix] in words_embed:
+            id2emb[ix] = words_embed[ix2word[ix]]
+        else:
+            id2emb[ix] = [1.0] * 50
+
+    data = [id2emb[ix] for ix in range(len(word2ix))]
+    return data
+
+
+def run():
+    useglove = True
+    df = pd.read_csv('dataset_features.csv')
+    feature_list = ['QE_Uncertainty', 'QE_Incongruity']
+
+    log_file = 'results-{}-{}.log'.format(useglove, strftime("%y%m%d-%H%M", localtime()))
+    logger.addHandler(logging.FileHandler(log_file))
+    if useglove:
+        dff = pd.read_csv('dataset.csv')
+        word2id = getWord2id(dff)
+        numpy_embed = get_numpy_word_embed(word2id)
+        embedding = nn.Embedding.from_pretrained(torch.FloatTensor(numpy_embed)).to('cuda')
+
+        setups = dff['set-up'].tolist()
+        punchlines = dff['punchline'].tolist()
+        setups_final_embed = []
+        punchlines_final_embed = []
+        for setup, punchline in zip(setups, punchlines):
+            setup = tokenize(setup)
+            setup_ids = [word2id.get(w) for w in setup]
+            setup_ids_tensor = torch.tensor(setup_ids).to('cuda')
+            setup_embedding = embedding(setup_ids_tensor)
+            setup_norm = torch.sum(setup_embedding, dim=0) / setup_embedding.shape[0]
+            setups_final_embed.append(setup_norm)
+
+            punchline = tokenize(punchline)
+            punchline_ids = [word2id.get(w) for w in punchline]
+            punchline_ids_tensor = torch.tensor(punchline_ids).to('cuda')
+            punchline_embedding = embedding(punchline_ids_tensor)
+            punchline_norm = torch.sum(punchline_embedding, dim=0) / punchline_embedding.shape[0]
+            punchlines_final_embed.append(punchline_norm)
+
+        setups_embed = torch.stack(setups_final_embed)
+        punchlines_embed = torch.stack(punchlines_final_embed)
+        word2vec_mat = torch.cat((setups_embed, punchlines_embed), dim=1)
+
+        p_list, r_list, f_list, acc_list = train_word2vec_svm_cv(df, 10, word2vec_mat, [])
+        logger.info('> feature is : {0}'.format('glove'))
+        logger.info('> p : {:.4f}'.format(p_list))
+        logger.info('> r : {:.4f}'.format(r_list))
+        logger.info('> f : {:.4f}'.format(f_list))
+        logger.info('> acc : {:.4f}'.format(acc_list))
+
+        for i in np.arange(len(feature_list)):
+            feature_combin_list = list(itertools.combinations(feature_list, i + 1))
+            for feature in feature_combin_list:
+                feature = list(feature)
+                p_list, r_list, f_list, acc_list = train_word2vec_svm_cv(df, 10, word2vec_mat, feature)
+                logger.info('> feature is : {0}'.format(feature))
+                logger.info('> p : {:.4f}'.format(p_list))
+                logger.info('> r : {:.4f}'.format(r_list))
+                logger.info('> f : {:.4f}'.format(f_list))
+                logger.info('> acc : {:.4f}'.format(acc_list))
+    else:
+        for i in np.arange(len(feature_list)):
+            feature_combin_list = list(itertools.combinations(feature_list, i + 1))
+            for feature in feature_combin_list:
+                feature = list(feature)
+                p_list, r_list, f_list, acc_list = train_svm_cv(df, 10, feature)
+                logger.info('> feature is : {0}'.format(feature))
+                logger.info('> p : {:.4f}'.format(p_list))
+                logger.info('> r : {:.4f}'.format(r_list))
+                logger.info('> f : {:.4f}'.format(f_list))
+                logger.info('> acc : {:.4f}'.format(acc_list))
+
+
+if __name__ == '__main__':
+    run()
---
+++ train.py
@@ -0,0 +1,160 @@
+import pandas as pd
+
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+
+import numpy as np
+import math
+from nltk import word_tokenize
+
+
+def getWord2id(df):
+    setups = df['set-up'].tolist()
+    punchlines = df['punchline'].tolist()
+    word2id = dict()
+    for setup, punchline in zip(setups, punchlines):
+        setup = word_tokenize(setup)
+        for s_word in setup:
+            s_word = s_word.lower().strip()
+            if word2id.get(s_word) == None:
+                word2id[s_word] = len(word2id)
+
+        punchline = word_tokenize(punchline)
+        for p_word in punchline:
+            p_word = p_word.lower().strip()
+            if word2id.get(p_word) == None:
+                word2id[p_word] = len(word2id)
+
+    return word2id
+
+
+def get_numpy_word_embed(word2ix):
+    row = 0
+    whole = 'glove.6B.50d.txt'
+    words_embed = {}
+    with open(whole, mode='r', encoding='utf-8')as f:
+        lines = f.readlines()
+        for line in lines:
+            line_list = line.split()
+            word = line_list[0]
+            embed = line_list[1:]
+            embed = [float(num) for num in embed]
+            norm = 0.0
+            for i in embed:
+                norm = norm + i * i
+            embed = [num / np.sqrt(norm) for num in embed]
+            words_embed[word] = embed
+            if row > 20000:
+                break
+            row += 1
+    ix2word = {ix: w for w, ix in word2ix.items()}
+    id2emb = {}
+    for ix in range(len(word2ix)):
+        if ix2word[ix] in words_embed:
+            id2emb[ix] = words_embed[ix2word[ix]]
+        else:
+            id2emb[ix] = [0.0] * 50
+    data = [id2emb[ix] for ix in range(len(word2ix))]
+
+    return data
+
+
+def getVonNeumannEntropy(word_density_matrix):
+    e_vs = torch.linalg.eigvals(word_density_matrix)
+    s_rho = torch.tensor(0.0).to('cuda')
+    for e_v in e_vs:
+        real_value = torch.view_as_real(e_v)[0]
+        if real_value.item() != 0:
+            s_rho_item = real_value * torch.log(real_value)
+            if torch.isnan(s_rho_item).item() is False:
+                s_rho += -s_rho_item
+    if torch.isnan(s_rho).item():
+        s_rho = torch.tensor(-1.0)
+    return s_rho.item()
+
+
+def getConditionalEntropy(rho_matrix, sigma_matrix):
+    sigma_rho_matrix = sigma_matrix * rho_matrix
+    sr_vs = torch.linalg.eigvals(sigma_rho_matrix)
+    s_rho_sigma = torch.tensor(0.0).to('cuda')
+    for sr_v in sr_vs:
+        sr_v_real = torch.view_as_real(sr_v)[0]
+        if sr_v_real.item() != 0:
+            s_rho_sigma_item = sr_v_real * torch.log(sr_v_real)
+            if torch.isnan(s_rho_sigma_item).item() is False:
+                s_rho_sigma += s_rho_sigma_item
+    e_vs = torch.linalg.eigvals(rho_matrix)
+    s_rho = torch.tensor(0.0).to('cuda')
+    for e_v in e_vs:
+        e_v_real = torch.view_as_real(e_v)[0]
+        if e_v_real.item() != 0:
+            s_rho_item = e_v_real * torch.log(e_v_real)
+            if torch.isnan(s_rho_item).item() is False:
+                s_rho += s_rho_item
+    c_v = s_rho_sigma - s_rho
+    if torch.isnan(c_v).item():
+        c_v = torch.tensor(-1.0)
+    return c_v.item()
+
+
+def isfloat(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+
+def get_features(df, embedding, word2id):
+    setups = df['set-up'].tolist()
+    punchlines = df['punchline'].tolist()
+    lb = df['label'].tolist()
+    qe_uncertainty_values = []
+    qe_incongruity_values = []
+    for setup, punchline in tqdm(zip(setups, punchlines), total=len(setups)):
+        setup = word_tokenize(setup)
+        setup_ids = [word2id.get(w.lower().strip()) for w in setup]
+        setup_ids_tensor = torch.tensor(setup_ids).to('cuda')
+        setup_embedding = embedding(setup_ids_tensor)
+
+        setup_matrix = torch.matmul(setup_embedding.unsqueeze(-1), setup_embedding.unsqueeze(-2))
+        setup_matrix = torch.mean(setup_matrix, dim=0)
+
+        punchline = word_tokenize(punchline)
+        punchline_ids = [word2id.get(w.lower().strip()) for w in punchline]
+        punchline_ids_tensor = torch.tensor(punchline_ids).to('cuda')
+        punchline_embedding = embedding(punchline_ids_tensor)
+
+        punchline_matrix = torch.matmul(punchline_embedding.unsqueeze(-1), punchline_embedding.unsqueeze(-2))
+        punchline_matrix = torch.mean(punchline_matrix, dim=0)
+
+        uncertainty = getVonNeumannEntropy(setup_matrix)
+
+        if math.isnan(uncertainty) or not isfloat(uncertainty):
+            uncertainty = torch.tensor(-1).item()
+
+        qe_uncertainty_values.append(uncertainty)
+
+        incongruity = getConditionalEntropy(setup_matrix, punchline_matrix)
+
+        if math.isnan(incongruity) or not isfloat(incongruity):  # 很少出现nan
+            incongruity = torch.tensor(-1).item()
+
+        qe_incongruity_values.append(incongruity)
+
+    return qe_uncertainty_values, qe_incongruity_values, lb
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('dataset.csv')
+    word2id = getWord2id(df)
+    numpy_embed = get_numpy_word_embed(word2id)
+    embedding = nn.Embedding.from_pretrained(torch.FloatTensor(numpy_embed)).to('cuda')
+    QE_Uncertainty, QE_Incongruity, labels = get_features(df, embedding, word2id)
+    pd.DataFrame({
+        'QE_Uncertainty': QE_Uncertainty,
+        'QE_Incongruity': QE_Incongruity,
+        'label': labels,
+    }).to_csv('dataset_features.csv', index=False)
